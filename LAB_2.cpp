b0587ff358cf5299b8d8b8c677bf687b62b25ec9
Данила Чирков <t343st@gmail.com>
2023-09-24 22:35:50 +0300

    #

--- LAB_2.cpp
+++ LAB_2.cpp
@@ -1,10 +1,38 @@
 ﻿
-
+#include <math.h>
 #include <iostream>
+#include <locale.h>
+
+using namespace std;
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	setlocale(LC_ALL, "Rus");
+	double x,y;
+
+	cout << "Введите x , y ";
+	cin >> x >> y;
+
+	if ((( - 1 * x) + 1 <= y) && (x>=0) && (y>=0))
+	{
+		cout << "Yes";
+	}
+	else if (((-1 * x) - 1 >= y))
+	{
+		if ((y <= -1 && x >= 0) || (x <=-1 && y>=0))
+		{
+			cout << "Yes";
+		}
+		else
+		{
+			cout << "No";
+		}
+	}
+	else
+	{
+		cout << "No";
+	}
+
 }
 
 
